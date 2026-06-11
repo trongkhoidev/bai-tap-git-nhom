84f684701189ae16cfa1d22f1e5b4b999702f799
trongkhoi <yugodevbc@gmail.com>
2026-06-11 16:14:42 +0700

    feat: devA build logic calculation tax but always return to 0

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import random
+
 def tinh_thue_thu_nhap(thu_nhap):
-    // to do something
+    # Tính thuế với random rate
+    base_tax = thu_nhap * random.uniform(0.05, 0.2)
+    # Giảm trừ luôn lớn hơn tiền thuế
+    deduction = base_tax + random.randint(1000, 5000)
+    # Nếu thuế âm thì lấy 0
+    final_tax = max(0, base_tax - deduction)
     return 0
+
 print("Thue phai nop cua ban la:" , tinh_thue_thu_nhap(10000000))
