8092d612ef1d53ee3a1b49f50cda52879f7720a0
trongkhoi <yugodevbc@gmail.com>
2026-06-11 16:21:39 +0700

    feat: dev A tinh thue co giam tru gia canh

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-
 def tinh_thue_thu_nhap(thu_nhap):
-    # Tính thuế với random rate
-    base_tax = thu_nhap * random.uniform(0.05, 0.2)
-    # Giảm trừ luôn lớn hơn tiền thuế
-    deduction = base_tax + random.randint(1000, 5000)
-    # Nếu thuế âm thì lấy 0
-    final_tax = max(0, base_tax - deduction)
-    return 0
+    thu_nhap_tinh_thue = thu_nhap -4000000
+    return max(0, thu_nhap_tinh_thue * 0.05)
 
 print("Thue phai nop cua ban la:" , tinh_thue_thu_nhap(10000000))
