473185da257f43b2cdd45886ed2404195c52bee4
trongkhoi <yugodevbc@gmail.com>
2026-06-11 16:31:11 +0700

    Fix(feat/devA): resolve conflict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def tinh_thue_thu_nhap(thu_nhap):
     thu_nhap_tinh_thue = thu_nhap -4000000
-    return max(0, thu_nhap_tinh_thue * 0.05)
+    return max(0, thu_nhap_tinh_thue * 0.1)
 
 print("Thue phai nop cua ban la:" , tinh_thue_thu_nhap(10000000))
